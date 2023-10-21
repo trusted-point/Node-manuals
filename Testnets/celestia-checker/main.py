0198ab968184aa3b90c8b552c0aba92745765a72
Hacker-web-Vi <80550154+Hacker-web-Vi@users.noreply.github.com>
2023-10-21 14:05:26 +0200

    Create main.py

---
+++ Testnets/celestia-checker/main.py
@@ -0,0 +1,31 @@
+python
+import json
+import requests
+from tabulate import tabulate
+
+with open("wallets.txt", "r") as file:
+    wallet_addresses = file.read().splitlines()
+
+url = "https://raw.githubusercontent.com/celestiaorg/networks/master/celestia/pre-genesis.json"
+response = requests.get(url)
+data = response.json()
+
+# Extract balances for each wallet address
+balances = []
+total_balance = 0
+
+for address in wallet_addresses:
+    found = [item for item in data["app_state"]["bank"]["balances"] if item["address"] == address]
+    if found:
+        balance = found[0]["coins"][0]
+        balance_str = int(balance['amount']) // 1000000
+        total_balance += balance_str
+        balances.append([address, balance_str])
+
+summary_line = ["Total $TIA", total_balance]
+balances.append(summary_line)
+
+headers = ["Address", "Balance in $TIA"]
+
+table = tabulate(balances, headers, tablefmt="grid")
+print(table)
