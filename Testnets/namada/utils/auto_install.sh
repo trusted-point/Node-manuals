398233974c38913192ff0780a2fc59b937aa8bc9
Roman <83868103+romanv1812@users.noreply.github.com>
2023-10-28 22:33:51 +0300

    Update auto_install.sh

--- Testnets/namada/utils/auto_install.sh
+++ Testnets/namada/utils/auto_install.sh
@@ -6,102 +6,65 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# Define log file
-LOG_FILE="./install.log"
-
-# Function to log messages
-log() {
-  echo "$1" >> "$LOG_FILE"
-}
-
 # Function to print info messages
 info() {
   echo -e "${BLUE}[INFO]${NC} $1"
-  log "[INFO] $1"
 }
 
 # Function to print success messages
 success() {
   echo -e "${GREEN}[SUCCESS]${NC} $1"
-  log "[SUCCESS] $1"
 }
 
 # Function to print error messages and exit
 error_exit() {
   echo -e "${RED}[ERROR]${NC} $1"
-  log "[ERROR] $1"
   exit 1
 }
 
-# Function to validate environment variables
-validate_var() {
-  if [[ ! "$1" =~ ^v[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
-    error_exit "Invalid version format for $2. Expected format is 'vX.Y.Z'."
+# Function to check the last command's exit status
+check_status() {
+  if [ $? -ne 0 ]; then
+    error_exit "$1"
   fi
 }
 
-# Validate environment variables
-validate_var "$NAMADA_TAG" "NAMADA_TAG"
-validate_var "$PROTOBUF_TAG" "PROTOBUF_TAG"
-validate_var "$COMETBFT_TAG" "COMETBFT_TAG"
-
-# Function to install a package
-install_package() {
-  local tag=$1
-  local url=$2
-  local tar_name=$3
-  local install_path=$4
-  local temp_folder=$5
-  local version_command=$6
-  local custom_commands=$7
-
-  info "Downloading package..."
-  curl -s -L -o "$tar_name" "$url"
-  if [ $? -ne 0 ]; then
-    error_exit "Failed to download package."
-  fi
-
-  if [[ "$custom_commands" != "none" ]]; then
-    mkdir $temp_folder && tar -xvf $tar_name -C $temp_folder/
-  else
-    tar -xvf $tar_name
-  fi
-
-  if [[ "$custom_commands" == "protobuf" ]]; then
-    sudo cp $temp_folder/bin/protoc $install_path
-    sudo cp -r $temp_folder/include/* /usr/local/include/
-  elif [[ "$custom_commands" == "cometbft" ]]; then
-    sudo mv $temp_folder/cometbft $install_path
-  else
-    sudo mv namada-${tag}-Linux-x86_64/* $install_path
-  fi
-
-  rm -rf $temp_folder $tar_name
-
-  local version=$($version_command 2>/dev/null)
-  if [ $? -ne 0 ]; then
-    error_exit "Failed to verify installation."
-  fi
-
-  success "$version successfully installed."
-}
-
-# Create a temporary directory and navigate into it
+# Create a temporary directory
 TMP_DIR=$(mktemp -d)
-if [ $? -ne 0 ]; then
-  error_exit "Failed to create a temporary directory."
-fi
-cd "$TMP_DIR"
+check_status "Failed to create a temporary directory."
+cd "$TMP_DIR" || error_exit "Failed to navigate to the temporary directory."
 
 # Install Namada
-install_package "$NAMADA_TAG" "https://github.com/anoma/namada/releases/download/$NAMADA_TAG/namada-${NAMADA_TAG}-Linux-x86_64.tar.gz" "namada.tar.gz" "/usr/local/bin" "none" "namada -V" "none"
+info "Installing Namada..."
+curl -s -L -o namada.tar.gz "https://github.com/anoma/namada/releases/download/$NAMADA_TAG/namada-${NAMADA_TAG}-Linux-x86_64.tar.gz"
+tar -xvf namada.tar.gz
+sudo mv namada-${NAMADA_TAG}-Linux-x86_64/* /usr/local/bin/
+rm -rf namada-${NAMADA_TAG}-Linux-x86_64 namada.tar.gz
+namada_version=$(namada -V 2>/dev/null)
+check_status "Failed to install Namada."
+success "$namada_version successfully installed."
 
 # Install Protocol Buffers
-install_package "$PROTOBUF_TAG" "https://github.com/protocolbuffers/protobuf/releases/download/$PROTOBUF_TAG/protoc-${PROTOBUF_TAG#v}-linux-x86_64.zip" "protobuf.zip" "/usr/local/bin" "protobuf_temp" "protoc --version" "protobuf"
+info "Installing Protocol Buffers..."
+curl -s -L -o protobuf.zip "https://github.com/protocolbuffers/protobuf/releases/download/$PROTOBUF_TAG/protoc-${PROTOBUF_TAG#v}-linux-x86_64.zip"
+mkdir protobuf_temp && unzip protobuf.zip -d protobuf_temp/
+sudo cp protobuf_temp/bin/protoc /usr/local/bin/
+sudo cp -r protobuf_temp/include/* /usr/local/include/
+rm -rf protobuf_temp protobuf.zip
+protobuf_version=$(protoc --version 2>/dev/null)
+check_status "Failed to install Protocol Buffers."
+success "$protobuf_version successfully installed."
 
 # Install CometBFT
-install_package "$COMETBFT_TAG" "https://github.com/cometbft/cometbft/releases/download/$COMETBFT_TAG/cometbft_${COMETBFT_TAG#v}_linux_amd64.tar.gz" "cometbft.tar.gz" "/usr/local/bin" "cometbft_temp" "cometbft --version" "cometbft"
+info "Installing CometBFT..."
+curl -s -L -o cometbft.tar.gz "https://github.com/cometbft/cometbft/releases/download/$COMETBFT_TAG/cometbft_${COMETBFT_TAG#v}_linux_amd64.tar.gz"
+mkdir cometbft_temp && tar -xvf cometbft.tar.gz -C cometbft_temp/
+sudo mv cometbft_temp/cometbft /usr/local/bin/
+rm -rf cometbft_temp cometbft.tar.gz
+cometbft_version=$(cometbft --version 2>/dev/null)
+check_status "Failed to install CometBFT."
+success "$cometbft_version successfully installed."
 
-# Cleanup and exit
+# Clean up
 cd - > /dev/null || error_exit "Failed to return to the original directory."
 rm -rf "$TMP_DIR"
