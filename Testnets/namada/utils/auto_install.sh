bfba0a63d1a9272c6b615997447df774283ca000
Roman <83868103+romanv1812@users.noreply.github.com>
2023-10-28 22:32:03 +0300

    Update auto_install.sh

--- Testnets/namada/utils/auto_install.sh
+++ Testnets/namada/utils/auto_install.sh
@@ -45,68 +45,63 @@ validate_var "$NAMADA_TAG" "NAMADA_TAG"
 validate_var "$PROTOBUF_TAG" "PROTOBUF_TAG"
 validate_var "$COMETBFT_TAG" "COMETBFT_TAG"
 
-# Your existing functions and main code here
-# ...
-
 # Function to install a package
 install_package() {
   local tag=$1
   local url=$2
   local tar_name=$3
   local install_path=$4
-  local version_command=$5
+  local temp_folder=$5
+  local version_command=$6
+  local custom_commands=$7
 
   info "Downloading package..."
   curl -s -L -o "$tar_name" "$url"
-  check_status "Failed to download package."
-
-  info "Extracting package..."
-  if [[ "$tar_name" == *.tar.gz ]]; then
-    tar -xf "$tar_name"
-  elif [[ "$tar_name" == *.zip ]]; then
-    unzip -q "$tar_name" -d "$install_path"
+  if [ $? -ne 0 ]; then
+    error_exit "Failed to download package."
   fi
-  check_status "Failed to extract package."
 
-  info "Installing package..."
-  if [[ "$tar_name" == *.tar.gz ]]; then
-    sudo mv "${tar_name%.tar.gz}"/* "$install_path"
+  if [[ "$custom_commands" != "none" ]]; then
+    mkdir $temp_folder && tar -xvf $tar_name -C $temp_folder/
+  else
+    tar -xvf $tar_name
   fi
-  check_status "Failed to install package."
 
-  info "Cleaning up..."
-  rm -rf "$tar_name" "${tar_name%.tar.gz}"
-  check_status "Failed to clean up."
+  if [[ "$custom_commands" == "protobuf" ]]; then
+    sudo cp $temp_folder/bin/protoc $install_path
+    sudo cp -r $temp_folder/include/* /usr/local/include/
+  elif [[ "$custom_commands" == "cometbft" ]]; then
+    sudo mv $temp_folder/cometbft $install_path
+  else
+    sudo mv namada-${tag}-Linux-x86_64/* $install_path
+  fi
 
-  info "Verifying installation..."
+  rm -rf $temp_folder $tar_name
+
   local version=$($version_command 2>/dev/null)
-  check_status "Failed to verify installation."
+  if [ $? -ne 0 ]; then
+    error_exit "Failed to verify installation."
+  fi
+
   success "$version successfully installed."
 }
 
-# Function to check the last command's exit status
-check_status() {
-  if [ $? -ne 0 ]; then
-    error_exit "$1"
-  fi
-}
-
-# Create a temporary directory
+# Create a temporary directory and navigate into it
 TMP_DIR=$(mktemp -d)
-check_status "Failed to create a temporary directory."
-cd "$TMP_DIR" || error_exit "Failed to navigate to the temporary directory."
+if [ $? -ne 0 ]; then
+  error_exit "Failed to create a temporary directory."
+fi
+cd "$TMP_DIR"
 
 # Install Namada
-install_package "$NAMADA_TAG" "https://github.com/anoma/namada/releases/download/$NAMADA_TAG/namada-${NAMADA_TAG}-Linux-x86_64.tar.gz" "namada.tar.gz" "/usr/local/bin" "namada -V"
+install_package "$NAMADA_TAG" "https://github.com/anoma/namada/releases/download/$NAMADA_TAG/namada-${NAMADA_TAG}-Linux-x86_64.tar.gz" "namada.tar.gz" "/usr/local/bin" "none" "namada -V" "none"
 
 # Install Protocol Buffers
-install_package "$PROTOBUF_TAG" "https://github.com/protocolbuffers/protobuf/releases/download/$PROTOBUF_TAG/protoc-${PROTOBUF_TAG#v}-linux-x86_64.zip" "protobuf.zip" "/usr/local" "protoc --version"
+install_package "$PROTOBUF_TAG" "https://github.com/protocolbuffers/protobuf/releases/download/$PROTOBUF_TAG/protoc-${PROTOBUF_TAG#v}-linux-x86_64.zip" "protobuf.zip" "/usr/local/bin" "protobuf_temp" "protoc --version" "protobuf"
 
 # Install CometBFT
-install_package "$COMETBFT_TAG" "https://github.com/cometbft/cometbft/releases/download/$COMETBFT_TAG/cometbft_${COMETBFT_TAG#v}_linux_amd64.tar.gz" "cometbft.tar.gz" "/usr/local/bin" "cometbft --version"
+install_package "$COMETBFT_TAG" "https://github.com/cometbft/cometbft/releases/download/$COMETBFT_TAG/cometbft_${COMETBFT_TAG#v}_linux_amd64.tar.gz" "cometbft.tar.gz" "/usr/local/bin" "cometbft_temp" "cometbft --version" "cometbft"
 
-# Clean up
+# Cleanup and exit
 cd - > /dev/null || error_exit "Failed to return to the original directory."
 rm -rf "$TMP_DIR"
-
-# End of script
