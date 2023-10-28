758fc028637d2744f831f61b2a436dc44f77f333
Roman <83868103+romanv1812@users.noreply.github.com>
2023-10-29 00:05:50 +0300

    Update auto_install.sh

--- Testnets/namada/utils/auto_install.sh
+++ Testnets/namada/utils/auto_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Логирование
+# Функции для логирования
 log_info() {
   echo -e "\033[36mℹ️ [$1]\033[0m $2"
 }
@@ -13,65 +13,60 @@ log_error() {
   echo -e "\033[31m✖ [$1]\033[0m $2"
 }
 
+# Проверка установленных переменных окружения
+if [ -z "$NAMADA_TAG" ] || [ -z "$PROTOBUF_TAG" ] || [ -z "$COMETBFT_TAG" ]; then
+  log_error "$(date '+%Y-%m-%dT%H:%M:%S')" "One or more tags are not set. Exiting."
+  exit 1
+fi
+
+# Функция для установки или обновления компонентов
 install_or_update() {
   local name=$1
-  local version=$2
+  local tag=$2
   local url=$3
-  local filetype=$4
-  local install_fn=$5
-
-  # Проверка на наличие тега
-  if [ -z "$version" ]; then
-    log_error "$(date '+%Y-%m-%dT%H:%M:%S')" "$name tag is not set. Skipping installation."
+  local install_fn=$4
+  
+  log_info "$(date '+%Y-%m-%dT%H:%M:%S')" "Checking installed version of $name..."
+  current_version=$($name --version 2>/dev/null | awk '{print $NF}')
+  
+  if [ "$current_version" == "$tag" ]; then
+    log_success "$(date '+%Y-%m-%dT%H:%M:%S')" "$name is already up-to-date ($tag)"
     return
   fi
-
-  # Проверка текущей установленной версии
-  current_version=$($name --version 2>/dev/null)
-
-  if [ "$current_version" == "$version" ]; then
-    log_success "$(date '+%Y-%m-%dT%H:%M:%S')" "$name is up-to-date. Version: $version"
+  
+  log_info "$(date '+%Y-%m-%dT%H:%M:%S')" "Downloading $name..."
+  if ! curl -L -o "${name}.tar.gz" "$url"; then
+    log_error "$(date '+%Y-%m-%dT%H:%M:%S')" "Failed to download $name"
     return
   fi
-
-  # Загрузка
-  log_info "$(date '+%Y-%m-%dT%H:%M:%S')" "Downloading $name from $url..."
-  curl -L -o "$name.$filetype" "$url"
-
-  # Установка
+  
   log_info "$(date '+%Y-%m-%dT%H:%M:%S')" "Installing $name..."
-  $install_fn "$name.$filetype"
-
-  # Проверка успешной установки
-  new_version=$($name --version 2>/dev/null)
-  if [ "$new_version" == "$version" ]; then
-    log_success "$(date '+%Y-%m-%dT%H:%M:%S')" "$name installed successfully. Version: $version"
-  else
-    log_error "$(date '+%Y-%m-%dT%H:%M:%S')" "Failed to install $name."
+  if ! $install_fn "${name}.tar.gz"; then
+    log_error "$(date '+%Y-%m-%dT%H:%M:%S')" "Failed to install $name"
+    return
   fi
+  
+  log_success "$(date '+%Y-%m-%dT%H:%M:%S')" "$name installed successfully ($tag)"
 }
 
-# Функции установки для каждого компонента
-install_namada_fn() {
-  tar -xvf $1 && sudo mv namada-${NAMADA_TAG}-Linux-x86_64/* /usr/local/bin/ && rm -rf namada-${NAMADA_TAG}-Linux-x86_64 $1
+# Функции для установки конкретных компонентов
+install_namada() {
+  tar -xvf $1 -C /usr/local/bin/
 }
 
-install_protobuf_fn() {
-  unzip -o $1 -d /usr/local/ && rm $1
+install_protobuf() {
+  unzip -o $1 -d /usr/local/
 }
 
-install_cometbft_fn() {
-  tar -xvf $1 && sudo mv cometbft /usr/local/bin/ && rm $1
+install_cometbft() {
+  tar -xvf $1 -C /usr/local/bin/
 }
 
-# Установка компонентов
-[ -n "$NAMADA_TAG" ] && \
-  install_or_update "namada" "$NAMADA_TAG" "https://github.com/anoma/namada/releases/download/$NAMADA_TAG/namada-${NAMADA_TAG}-Linux-x86_64.tar.gz" "tar.gz" install_namada_fn
+# Установка или обновление компонентов
+install_or_update "namada" "$NAMADA_TAG" "https://github.com/anoma/namada/releases/download/$NAMADA_TAG/namada-${NAMADA_TAG}-Linux-x86_64.tar.gz" install_namada
 
-[ -n "$PROTOBUF_TAG" ] && \
-  install_or_update "protoc" "$PROTOBUF_TAG" "https://github.com/protocolbuffers/protobuf/releases/download/$PROTOBUF_TAG/protoc-${PROTOBUF_TAG#v}-linux-x86_64.zip" "zip" install_protobuf_fn
+install_or_update "protoc" "$PROTOBUF_TAG" "https://github.com/protocolbuffers/protobuf/releases/download/$PROTOBUF_TAG/protoc-${PROTOBUF_TAG#v}-linux-x86_64.zip" install_protobuf
 
-[ -n "$COMETBFT_TAG" ] && \
-  install_or_update "cometbft" "$COMETBFT_TAG" "https://github.com/cometbft/cometbft/releases/download/$COMETBFT_TAG/cometbft_${COMETBFT_TAG#v}_linux_amd64.tar.gz" "tar.gz" install_cometbft_fn
+install_or_update "cometbft" "$COMETBFT_TAG" "https://github.com/cometbft/cometbft/releases/download/$COMETBFT_TAG/cometbft_${COMETBFT_TAG#v}_linux_amd64.tar.gz" install_cometbft
 
-log_info "$(date '+%Y-%m-%dT%H:%M:%S')" "🎉 Installation or update completed! Check the log file for more details."
+log_info "$(date '+%Y-%m-%dT%H:%M:%S')" "🎉 Installation or update completed!"
